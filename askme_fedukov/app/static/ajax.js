e5cc07a02e4cfc620fa69b95058390d9d770685a
Ubuntu Desktop <orlando20056@yandex.ru>
2025-05-25 18:08:37 +0300

    fix: dynamic layout scrollfit

--- askme_fedukov/app/static/ajax.js
+++ askme_fedukov/app/static/ajax.js
@@ -1,2 +1,27 @@
-const axios = require('axios').default;
+function name(params) {
+    axios.get('/profile/0/', {
+        params: {
+            id: 12345
+        }
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function () {
+        console.log('Request completed');
+    });
+}
 
+function main() {
+    // All axios requests will have csrf token in headers
+    axios.defaults.headers.common['X-CSRFToken'] = Cookies.get("csrftoken");
+
+
+}
+
+
+document.addEventListener('DOMContentLoaded', main)
+    
